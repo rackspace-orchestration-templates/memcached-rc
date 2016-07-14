05cc0b652a782f1ce593ae145530341e80ee79e0
Chris Hultin <chris.hultin@rackspace.com>
2016-07-14 16:23:46 -0500

    Initial commit

---
+++ test/fabric/memcached_server.py
@@ -0,0 +1,20 @@
+from fabric.api import env, task
+from envassert import detect, file, package, port, process, service
+from hot.utils.test import get_artifacts
+
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert package.installed("memcached")
+    assert file.exists("/etc/memcached.conf")
+    assert port.is_listening(11211)
+    assert process.is_up("memcached")
+    assert service.is_enabled("memcached")
+
+
+@task
+def artifacts():
+    env.platform_family = detect.detect()
+    get_artifacts()
